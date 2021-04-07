8c5240bf790cabc69eb9c417676faabd519fd601
Daniel Kosykh <dankosykh@gmail.com>
2021-04-06 17:52:00 -0700

    Complete routes to DB from server (findall, update, delete, post, create) (#5)
    * init resume and job listing schemas
    
    * complete job listing model
    
    * complete models and initial tests
    
    * add server controllers and db controllers
    
    * Added mock data, added tests for all resumes, job listings, and employee notes. Added routes for listings and resumes. Added controller and models for sub documents of notes and application. Changed existing user models to destructure subdocuments.
    
    * refactor add subdocument for all models
    
    * refactor delete subdocument for all models
    
    * refactor create model and update model, refactored code to utilize common functions
    
    * PR requested changes
    
    * server routes update
    
    * check against daniel pr
    
    * changed .env example fields
    
    * created findall for subdocuments and working post responses for notes and appointments
    
    * reworked and tested note crud, appointment crud, application crud, and saved jobs post and get all
    
    Co-authored-by: Michael Smith <mijamessmith@gmail.com>

---
+++ database/controller/resume.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const { createModel } = require('./reuse.js');
+const ResumeModel = require('../model/resumeModel.js');
+
+const resume = {
+
+  createOne: (params) => {
+    return new Promise ((resolve, reject) => {
+      createModel(ResumeModel, params, resolve, reject);
+    });
+  },
+
+  findOne: (seekerId) => {
+    return new Promise((resolve, reject) => {
+      ResumeModel.find({seekerId: seekerId})
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          reject(err);
+        });
+      });
+  },
+  deleteOne: (id) => {
+    return new Promise((resolve, reject) => {
+      ResumeModel.deleteOne({_id: id})
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          reject(err);
+        });
+      });
+  },
+  updateOne: (resumeId) => {
+    return new Promise((resolve, reject) => {
+      ResumeModel.updateOne({_id: resumeId})
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          reject(err);
+        });
+      });
+  },
+  findAll: () => {
+    return new Promise((resolve, reject) => {
+      ResumeModel.find({})
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          reject(err);
+        });
+      });
+  },
+  findAllByFilter: (params) => {
+    return new Promise((resolve, reject) => {
+      ResumeModel.find(params)
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          reject(err);
+        });
+      });
+  }
+}
+
+module.exports = resume;
