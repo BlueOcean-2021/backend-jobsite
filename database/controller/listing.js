1a12741223052d2cb4475c3fce85ac2e0c396fec
Daniel Kosykh <dankosykh@gmail.com>
2021-04-08 16:36:20 -0700

    fix bug

--- database/controller/listing.js
+++ database/controller/listing.js
@@ -49,7 +49,7 @@ const listing = {
   // delete one by listing _id
   deleteOne: (jobListingId) => {
     return new Promise((resolve, reject) => {
-      jobListingMode.deleteOne({_id: jobListingId})
+      jobListingModel.deleteOne({_id: jobListingId})
         .then(result => resolve(result))
         .catch(err => reject(err));
     });
